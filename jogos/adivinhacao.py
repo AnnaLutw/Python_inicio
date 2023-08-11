8b09be0e30f799296bed4ddabb2355ab5c16f1a9
annalutw <correioaninha@gmail.com>
2023-08-11 19:39:21 -0300

    python commit

--- jogos/adivinhacao.py
+++ jogos/adivinhacao.py
@@ -1,11 +1,28 @@
+import random
+
 print("*********************************")
 print("Bem vindo no jogo de adivinhação")
 print("*********************************")
 
-numero_secreto = 43
-total_de_tentativas = 3
-rodada = 1
+numero_secreto = round(random.randrange(1, 101))
+total_de_tentativas = 0
+pontos = 1000
 
+print("Qual nivel de difiuldade?")
+print("(1)- Facil")
+print("(2)- Medio")
+print("(3)- Dificill")
+
+nivel = int(input("Define o nível: "))
+
+if(nivel == 1):
+    total_de_tentativas = 20
+elif(nivel == 2):
+    total_de_tentativas = 10
+else:
+    total_de_tentativas = 5
+
+print("Você tem 1000 pontos iniciais")
 for rodada in range (1, total_de_tentativas + 1):
     print("*********************************")
     print("Tentativa:  {}  de  {}  ".format(rodada, total_de_tentativas ))
@@ -23,13 +40,14 @@ for rodada in range (1, total_de_tentativas + 1):
 
     if(acertou):
         print("VOCÊ ACERTOU, PARABENS!!")
+        print("** Fez {} pontos **".format(pontos))
         break
     else:
         if(maior):
             print("Você errou *-*. O seu chute foi MAIOR do que o número secreto")
         elif(menor):
             print("Você errou *-*. O seu chute foi MENOR do que o número secreto")
+        pontos_perdidos = abs(numero_secreto - chute)
+        pontos = pontos - pontos_perdidos
 
 print("FIM DE JOGO")
-
-
